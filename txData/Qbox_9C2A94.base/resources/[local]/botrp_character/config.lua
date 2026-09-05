e6255382b2110918d9ab3115df2c82b012a1cc55
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-06 03:42:10 +0530

    feat: configure BotRP character foundation

---
+++ txData/Qbox_9C2A94.base/resources/[local]/botrp_character/config.lua
@@ -0,0 +1,18 @@
+Config = {
+    ServerName = 'BotRP',
+    Version = '0.2.0',
+
+    MaxVisibleCharacters = 6,
+    DeleteCharacters = true,
+
+    -- Reuse Qbox's existing spawn resources. No second spawn implementation is created here.
+    PreferApartments = true,
+    PreferQbxSpawn = true,
+
+    UI = {
+        Accent = '#21a8ff',
+        AccentSoft = '#0d6fa8',
+        Background = '#070b11',
+        Card = '#0c121a',
+    }
+}
