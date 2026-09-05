e3f41029a818fda890fc40fb1c4de3b99083c128
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-05 21:00:11 +0530

    brand: align qbx core server identity with NoPixel India

--- txData/Qbox_9C2A94.base/resources/[qbx]/qbx_core/config/shared.lua
+++ txData/Qbox_9C2A94.base/resources/[qbx]/qbx_core/config/shared.lua
@@ -1,9 +1,9 @@
 return {
-    serverName = 'Server',
+    serverName = 'NoPixel India',
     defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
-    notifyPosition = 'top-right', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
+    notifyPosition = 'top-right',
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
-    starterItems = { -- Character starting items
+    starterItems = {
         { name = 'phone', amount = 1 },
         { name = 'id_card', amount = 1, metadata = function(source)
                 assert(GetResourceState('qbx_idcard') == 'started', 'qbx_idcard resource not found. Required to give an id_card as a starting item')
@@ -11,7 +11,7 @@ return {
             end
         },
         { name = 'driver_license', amount = 1, metadata = function(source)
-                assert(GetResourceState('qbx_idcard') == 'started', 'qbx_idcard resource not found. Required to give an id_card as a starting item')
+                assert(GetResourceState('qbx_idcard') == 'started', 'qbx_idcard resource not found. Required to give a driver_license as a starting item')
                 return exports.qbx_idcard:GetMetaLicense(source, {'driver_license'})
             end
         },
