69a13181c96001fae12a5d82a692379e6d4b567b
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-06 04:21:37 +0530

    Add BotRP loadscreen safety resource

---
+++ txData/Qbox_9C2A94.base/resources/[local]/botrp_loadscreenfix/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'botrp_loadscreenfix'
+description 'BotRP loading-screen shutdown safety bridge for external character management'
+version '1.0.0'
+
+client_script 'client.lua'
+
+dependencies {
+    'qbx_core'
+}
