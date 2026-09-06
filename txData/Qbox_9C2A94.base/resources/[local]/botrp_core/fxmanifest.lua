49d87c38a8c5e0eb2c195ca1a05bb5b1d299043d
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-07 02:10:56 +0530

    feat(player): register BotRP identity foundation

--- txData/Qbox_9C2A94.base/resources/[local]/botrp_core/fxmanifest.lua
+++ txData/Qbox_9C2A94.base/resources/[local]/botrp_core/fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'botrp_core'
 author 'BotRP Development'
 description 'BotRP player experience foundation'
-version '0.3.3'
+version '0.4.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -14,11 +14,13 @@ shared_scripts {
 }
 
 client_scripts {
+    'client/identity.lua',
     'client/main.lua',
     'client/onboarding.lua'
 }
 
 server_scripts {
+    'server/identity.lua',
     'server/main.lua'
 }
 
